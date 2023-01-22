df22c581bf6009e625a9c528edea27e5d036b94e
SampritiSoor <44729873+SampritiSoor@users.noreply.github.com>
2023-01-22 16:49:51 +0530

    Add files via upload

--- CRISMrelatedMethods/score.py
+++ CRISMrelatedMethods/score.py
@@ -6,6 +6,16 @@ mineralGroupList=getVarInfo('./specifications.z','mineralGroupList')
 mineralGroupDict=getVarInfo('./specifications.z','mineralGroupDict')
 mineralIndexMap=getVarInfo('./specifications.z','mineralIndexMap')
 
+def shuffle_in_unison(a, b):
+    assert len(a) == len(b)
+    shuffled_a = np.empty(a.shape, dtype=a.dtype)
+    shuffled_b = np.empty(b.shape, dtype=b.dtype)
+    permutation = np.random.permutation(len(a))
+    for old_index, new_index in enumerate(permutation):
+        shuffled_a[new_index] = a[old_index]
+        shuffled_b[new_index] = b[old_index]
+    return shuffled_a, shuffled_b
+
 def getGroupPred(pred):        
     return np.array([mineralGroupDict[p] for p in pred])
 def getFfoldFitScore(trainedModel,testDataX,testDataY,f=5,groupResult=True,foldScore=False):
